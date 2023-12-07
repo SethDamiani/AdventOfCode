1abf92d71bad12555cd05e0a04f709ac2037ac3c
Seth Damiani <SethDamiani@users.noreply.github.com>
2023-12-07 00:05:18 -0500

    5

--- 2023/day5.py
+++ 2023/day5.py
@@ -7,91 +7,102 @@ seeds = [int(s) for s in seeds]
 
 print("Get soil")
 i = 3
-seed_to_soil = {}
+seed_to_soil = []
 while len(text[i]) > 0:
-    dest_start, source_start, length = [int(s) for s in text[i].split(" ")]
-    for n in range(length):
-        seed_to_soil[source_start] = dest_start
-        source_start += 1
-        dest_start += 1
+    seed_to_soil.append([int(s) for s in text[i].split(" ")])
     i += 1
 
 print("Get fertilizer")
 i += 2
-soil_to_fertilizer = {}
+soil_to_fertilizer = []
 while len(text[i]) > 0:
-    dest_start, source_start, length = [int(s) for s in text[i].split(" ")]
-    for n in range(length):
-        soil_to_fertilizer[source_start] = dest_start
-        source_start += 1
-        dest_start += 1
+    soil_to_fertilizer.append([int(s) for s in text[i].split(" ")])
     i += 1
 
 print("Get water")
 i += 2
-fertilizer_to_water = {}
+fertilizer_to_water = []
 while len(text[i]) > 0:
-    dest_start, source_start, length = [int(s) for s in text[i].split(" ")]
-    for n in range(length):
-        fertilizer_to_water[source_start] = dest_start
-        source_start += 1
-        dest_start += 1
+    fertilizer_to_water.append([int(s) for s in text[i].split(" ")])
     i += 1
 
 print("Get light")
 i += 2
-water_to_light = {}
+water_to_light = []
 while len(text[i]) > 0:
-    dest_start, source_start, length = [int(s) for s in text[i].split(" ")]
-    for n in range(length):
-        water_to_light[source_start] = dest_start
-        source_start += 1
-        dest_start += 1
+    water_to_light.append([int(s) for s in text[i].split(" ")])
     i += 1
 
 print("Get temperature")
 i += 2
-light_to_temperature = {}
+light_to_temperature = []
 while len(text[i]) > 0:
-    dest_start, source_start, length = [int(s) for s in text[i].split(" ")]
-    for n in range(length):
-        light_to_temperature[source_start] = dest_start
-        source_start += 1
-        dest_start += 1
+    light_to_temperature.append([int(s) for s in text[i].split(" ")])
     i += 1
 
 print("Get humidity")
 i += 2
-temperature_to_humidity = {}
+temperature_to_humidity = []
 while len(text[i]) > 0:
-    dest_start, source_start, length = [int(s) for s in text[i].split(" ")]
-    for n in range(length):
-        temperature_to_humidity[source_start] = dest_start
-        source_start += 1
-        dest_start += 1
+    temperature_to_humidity.append([int(s) for s in text[i].split(" ")])
     i += 1
 
 print("Get location")
 i += 2
-humidity_to_location = {}
+humidity_to_location = []
 while i < len(text) and len(text[i]) > 0:
-    dest_start, source_start, length = [int(s) for s in text[i].split(" ")]
-    for n in range(length):
-        humidity_to_location[source_start] = dest_start
-        source_start += 1
-        dest_start += 1
+    humidity_to_location.append([int(s) for s in text[i].split(" ")])
     i += 1
 
+master_map = {
+    "seed_to_soil": {},
+    "soil_to_fertilizer": {},
+    "fertilizer_to_water": {},
+    "water_to_light": {},
+    "light_to_temperature": {},
+    "temperature_to_humidity": {},
+    "humidity_to_location": {},
+}
+def translate(map, value, name):
+    if value in master_map[name]:
+        return master_map[name][value]
+    for m in map:
+        if m[1] <= value <= m[1]+m[2]:
+            result = m[0] + value - m[1]
+            master_map[name][value] = result
+            return result
+    master_map[name][value] = value
+    return value
+
+done_map = {}
+def seed_to_location(seed):
+    if seed in done_map:
+        return done_map[seed]
+    soil = translate(seed_to_soil, seed, "seed_to_soil")
+    fertilizer = translate(soil_to_fertilizer, soil, "soil_to_fertilizer")
+    water = translate(fertilizer_to_water, fertilizer, "fertilizer_to_water")
+    light = translate(water_to_light, water, "water_to_light")
+    temperature = translate(light_to_temperature, light, "light_to_temperature")
+    humidity = translate(temperature_to_humidity, temperature, "temperature_to_humidity")
+    location = translate(humidity_to_location, humidity, "humidity_to_location")
+    done_map[seed] = location
+    return location
+
 print("Begin Part 1")
-part1 = 999999
+part1 = 999999999999999999999999999999
 for seed in seeds:
-    soil = seed_to_soil[seed] if seed in seed_to_soil else seed
-    fertilizer = soil_to_fertilizer[soil] if soil in soil_to_fertilizer else soil
-    water = fertilizer_to_water[fertilizer] if fertilizer in fertilizer_to_water else fertilizer
-    light = water_to_light[water] if water in water_to_light else water
-    temperature = light_to_temperature[light] if light in light_to_temperature else light
-    humidity = temperature_to_humidity[temperature] if temperature in temperature_to_humidity else temperature
-    location = humidity_to_location[humidity] if humidity in humidity_to_location else humidity
+    location = seed_to_location(seed)
+    #print(seed, soil, fertilizer, water, light, temperature, humidity, location)
     if location < part1:
         part1 = location
 print(part1)
+
+part2 = 999999999999999999999999999999
+for n in range(0, len(seeds), 2):
+    print(f"Part 2: {seeds[n]} to {seeds[n]+seeds[n+1]}")
+    for i in range(seeds[n+1]):
+        seed = seeds[n] + i
+        location = seed_to_location(seed)
+        if location < part2:
+            part2 = location
+print(part2)
